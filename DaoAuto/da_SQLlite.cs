544d9623f4472c853face4e76df7c63717bb39a9
o0ocuchuoio0o <tranthangtin1@gmail.com>
2020-01-07 15:10:09 +0700

    a
    a

---
+++ DaoAuto/da_SQLlite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+using System.Data;
+
+namespace DaoAuto
+{
+    public class da_SQLlite
+    {
+        public static SQLiteConnection hamketnoisql()
+        {
+            return new SQLiteConnection("Data Source=AutoF.sqlite;Version=3;");
+        }
+
+
+        public DataTable DanhSachTK(SQLiteConnection conn)
+        {
+            DataSet ds = new DataSet();
+            conn.Open();
+            SQLiteDataAdapter da = new SQLiteDataAdapter("select * from tbl_taikhoan", conn);          
+            try
+            {
+              
+                da.Fill(ds, "DanhSach");
+                return ds.Tables["DanhSach"];
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {               
+                conn.Close();
+                conn.Dispose();
+            }
+           
+        }
+    }
+}
